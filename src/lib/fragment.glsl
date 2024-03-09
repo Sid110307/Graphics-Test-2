023731f9b6bb3030c0bd47262217f55e28a79583
Sid110307 <srsfool@gmail.com>
2024-03-09 12:59:31 +0530

    Fixed bugs

--- src/lib/fragment.glsl
+++ src/lib/fragment.glsl
@@ -1,11 +1,11 @@
 #version 330 core
 
-in vec2 texCoords;
+in vec2 textureCoords;
 out vec4 color;
 
 uniform sampler2D texture_diffuse;
 
 void main()
 {
-    color = vec4(texture(texture_diffuse, texCoords));
+    color = vec4(texture(texture_diffuse, textureCoords));
 }
